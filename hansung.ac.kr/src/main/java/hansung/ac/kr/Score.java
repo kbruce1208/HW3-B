acb768b25dd95d40910e9d97bbe5e0485cfb917e
kbruce1208 <bruce1993@naver.com>
2017-04-18 21:27:57 +0900

    commit 4

--- hansung.ac.kr/src/main/java/hansung/ac/kr/Score.java
+++ hansung.ac.kr/src/main/java/hansung/ac/kr/Score.java
@@ -1,13 +1,1 @@
-package hansung.ac.kr;
-
-public class Score { 
-	private int m;s
-	private int f;
-	private int h;
-	private String g;
-	
-	public Score(int m,int f, int h)throws InvalidScoreException(){
-		if(m<0 || m>35)throw new InvalidScoreException();
-	}
-test git!!dwdw
-}ssss
+commit 4
